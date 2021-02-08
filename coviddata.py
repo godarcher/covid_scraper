55cbb14a78019b714c595d05ffc25f295c4bb00b
Joost Grunwald <joostgrunwald2001@gmail.com>
2021-02-08 20:27:28 +0100

    added list country per element functionality

--- coviddata.py
+++ coviddata.py
@@ -4,6 +4,12 @@
 # covid dependency
 from covid import Covid
 
+#?###########
+#* Settings #
+#?###########
+
+show_countries = True
+
 # We initialize covid
 covid = Covid()
 
@@ -12,9 +18,15 @@ print("Total active cases in world:", covid.get_total_active_cases())
 print("Total recovered cases in world:", covid.get_total_recovered())
 print("Total deaths in world:", covid.get_total_deaths())
 
+if show_countries == True:
+    countries = covid.list_countries()
+    for x in countries:
+        print(x)
+
 # get specific country related covid stats
-covid.list_countries()
-#cases = covid.get_status_by_country_name("nl")
+cases_nl = covid.get_status_by_country_name("Netherlands")
+cases_eng = covid.
+United Kingdom
 
 # printing country's data using for loop
 for x in cases:
