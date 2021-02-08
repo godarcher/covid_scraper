8c8fbc58e6473c50c45bf6e39e79f8ece0074dee
Joost Grunwald <joostgrunwald2001@gmail.com>
2021-02-08 20:31:53 +0100

    added confirmed cases

--- coviddata.py
+++ coviddata.py
@@ -8,15 +8,18 @@ from covid import Covid
 #* Settings #
 #?###########
 
-show_countries = True
+show_countries = False
 
 # We initialize covid
 covid = Covid()
 
 # Printing global data
+print("")
+print("Total confirmed cases in world:", covid.get_total_confirmed_cases())
 print("Total active cases in world:", covid.get_total_active_cases())
 print("Total recovered cases in world:", covid.get_total_recovered())
 print("Total deaths in world:", covid.get_total_deaths())
+covid.get_total_confirmed_cases
 
 if show_countries == True:
     countries = covid.list_countries()
@@ -32,4 +35,4 @@ cases_france = covid.get_status_by_country_name("France")
 
 # printing country's data using for loop
 for x in cases_nl:
-    print(x, ":", cases[x])
+    print(x, ":", cases_nl[x])
