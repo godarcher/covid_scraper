7b825de324ea0dbaf821854a2abf2143f053290a
Joost Grunwald <joostgrunwald2001@gmail.com>
2021-02-08 20:35:47 +0100

    started using integers to fetch global data

--- coviddata.py
+++ coviddata.py
@@ -13,13 +13,18 @@ show_countries = False
 # We initialize covid
 covid = Covid()
 
+# initializing integers to work with data
+confirmed_cases = covid.get_total_confirmed_cases()
+active_cases = covid.get_total_active_cases()
+recovered_cases = covid.get_total_recovered()
+total_deaths = covid.get_total_deaths()
+
 # Printing global data
 print("")
-print("Total confirmed cases in world:", covid.get_total_confirmed_cases())
-print("Total active cases in world:", covid.get_total_active_cases())
-print("Total recovered cases in world:", covid.get_total_recovered())
-print("Total deaths in world:", covid.get_total_deaths())
-covid.get_total_confirmed_cases
+print("Total confirmed cases in world:", confirmed_cases)
+print("Total active cases in world:", active_cases)
+print("Total recovered cases in world:", recovered_cases)
+print("Total deaths in world:", total_deaths)
 
 if show_countries == True:
     countries = covid.list_countries()
@@ -33,6 +38,7 @@ cases_bel = covid.get_status_by_country_name("Belgium")
 cases_ger = covid.get_status_by_country_name("Germany")
 cases_france = covid.get_status_by_country_name("France")
 
-# printing country's data using for loop
+# printing country's data using for loope
+print("")
 for x in cases_nl:
     print(x, ":", cases_nl[x])
